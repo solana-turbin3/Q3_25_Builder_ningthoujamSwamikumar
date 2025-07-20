abdcb962fa25bb8d50ef0df992b3a047d0424cd4
Swamikumar <nswamikumar@gmail.com>
2025-07-20 11:51:06 +0000

    working escrow and tested

--- week2/escrow/programs/escrow/src/lib.rs
+++ week2/escrow/programs/escrow/src/lib.rs
@@ -1,13 +1,14 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
+    token_interface, //::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked, close_account, CloseAccount},
 };
 
 declare_id!("6Cd8qb7i2pknKX2VyHUZLJJJUCarhw9Eg26qfd7FXus8");
 
 #[program]
 pub mod escrow {
+
     use super::*;
 
     pub fn initialize(
@@ -31,7 +32,7 @@ pub mod escrow {
 
         //transfer the token amount from users account to vault, in our case that would be offer_ata
         //construct cpi context, and do a cpi call
-        let transfer_accounts = TransferChecked {
+        let transfer_accounts = token_interface::TransferChecked {
             authority: acc.initializer.to_account_info(),
             from: acc.initializer_ata.to_account_info(),
             to: acc.vault.to_account_info(),
@@ -40,20 +41,20 @@ pub mod escrow {
 
         let cpi_ctx = CpiContext::new(acc.token_program.to_account_info(), transfer_accounts);
 
-        transfer_checked(cpi_ctx, offer_amnt, acc.offer_mint.decimals)
+        token_interface::transfer_checked(cpi_ctx, offer_amnt, acc.offer_mint.decimals)
     }
 
     pub fn accept(ctx: Context<Accept>, offer_id: u64) -> Result<()> {
         let accnts = ctx.accounts;
 
-        //transfer asked token from acceptors sending ata to initiators recieving ata
-        transfer_checked(
+        //transfer asked token from acceptors sending ata to initializers recieving ata
+        token_interface::transfer_checked(
             CpiContext::new(
                 accnts.token_program.to_account_info(),
-                TransferChecked {
+                token_interface::TransferChecked {
                     from: accnts.acceptors_sending_ata.to_account_info(),
                     mint: accnts.asked_mint.to_account_info(),
-                    to: accnts.initiators_recieving_ata.to_account_info(),
+                    to: accnts.initializers_recieving_ata.to_account_info(),
                     authority: accnts.acceptor.to_account_info(),
                 },
             ),
@@ -61,15 +62,17 @@ pub mod escrow {
             accnts.asked_mint.decimals,
         )?;
 
+        msg!("transferred asked token from acceptor to initiator");
+
         //transfer offered token from vault to acceptors recieving ata
-        transfer_checked(
+        token_interface::transfer_checked(
             CpiContext::new_with_signer(
                 accnts.token_program.to_account_info(),
-                TransferChecked {
+                token_interface::TransferChecked {
                     from: accnts.vault.to_account_info(),
-                    mint: accnts.asked_mint.to_account_info(),
+                    mint: accnts.offered_mint.to_account_info(),
                     to: accnts.acceptors_recieving_ata.to_account_info(),
-                    authority: accnts.acceptor.to_account_info(),
+                    authority: accnts.offer.to_account_info(),
                 },
                 &[&[
                     b"offer".as_ref(),
@@ -79,7 +82,47 @@ pub mod escrow {
             ),
             accnts.offer.offer_amount,
             accnts.asked_mint.decimals,
-        )
+        )?;
+
+
+        msg!("transferred offered token from vault to acceptors recieving ata");
+
+        //close the vault
+        //transfer any remaining tokens if any to the initializer
+        if accnts.vault.amount - accnts.offer.offer_amount > 0 {
+            token_interface::transfer_checked(
+                CpiContext::new_with_signer(
+                    accnts.token_program.to_account_info(),
+                    token_interface::TransferChecked {
+                        from: accnts.vault.to_account_info(),
+                        mint: accnts.offered_mint.to_account_info(),
+                        to: accnts.initializers_sending_ata.to_account_info(),
+                        authority: accnts.offer.to_account_info(),
+                    },
+                    &[&[
+                        b"offer".as_ref(),
+                        &offer_id.to_le_bytes().as_ref(),
+                        &[ctx.bumps.offer],
+                    ]],
+                ),
+                accnts.vault.amount,
+                accnts.offered_mint.decimals,
+            )?;
+        }
+        //close the vault account
+        token_interface::close_account(CpiContext::new_with_signer(
+            accnts.token_program.to_account_info(),
+            token_interface::CloseAccount {
+                account: accnts.vault.to_account_info(),
+                authority: accnts.offer.to_account_info(),
+                destination: accnts.initializer.to_account_info(),
+            },
+            &[&[
+                b"offer",
+                &offer_id.to_le_bytes().as_ref(),
+                &[ctx.bumps.offer],
+            ]],
+        ))
     }
 }
 
@@ -89,53 +132,73 @@ pub struct Accept<'info> {
     #[account(mut)]
     pub acceptor: Signer<'info>,
 
+    #[account(mut)]
+    pub initializer: SystemAccount<'info>,
+
     #[account(
+        mut,
         seeds = [b"offer", offer_id.to_le_bytes().as_ref()],
         bump,
+        close = initializer,
     )]
     pub offer: Account<'info, Offer>,
 
     #[account(
+        mut,
         associated_token::authority = offer,
         associated_token::token_program = token_program,
-        associated_token::mint = offer.offer_mint,
+        associated_token::mint = offer.offer_mint
     )]
-    pub vault: InterfaceAccount<'info, TokenAccount>,
+    pub vault: InterfaceAccount<'info, token_interface::TokenAccount>,
 
     #[account(
+        mut,
         associated_token::authority = acceptor,
         associated_token::mint = offer.ask_mint,
         associated_token::token_program = token_program,
     )]
-    pub acceptors_sending_ata: InterfaceAccount<'info, TokenAccount>,
+    pub acceptors_sending_ata: InterfaceAccount<'info, token_interface::TokenAccount>,
 
     #[account(
-        associated_token::mint = offer.offer_mint,
+        init_if_needed,
+        payer = acceptor,
+        associated_token::mint = offered_mint,
         associated_token::authority = acceptor,
         associated_token::token_program = token_program,
     )]
-    pub acceptors_recieving_ata: InterfaceAccount<'info, TokenAccount>,
+    pub acceptors_recieving_ata: InterfaceAccount<'info, token_interface::TokenAccount>,
 
     #[account(
-        associated_token::mint = offer.ask_mint,
-        associated_token::authority = offer.user,
+        init_if_needed,
+        payer = acceptor,
+        associated_token::mint = asked_mint,
+        associated_token::authority = initializer,
         associated_token::token_program = token_program,
     )]
-    pub initiators_recieving_ata: InterfaceAccount<'info, TokenAccount>,
+    pub initializers_recieving_ata: InterfaceAccount<'info, token_interface::TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = offered_mint,
+        associated_token::authority = initializer,
+        associated_token::token_program = token_program,
+    )]
+    pub initializers_sending_ata: InterfaceAccount<'info, token_interface::TokenAccount>,
 
     #[account(
         mint::token_program = token_program,
         address = offer.offer_mint,
     )]
-    pub offered_mint: InterfaceAccount<'info, Mint>,
+    pub offered_mint: InterfaceAccount<'info, token_interface::Mint>,
 
     #[account(
         mint::token_program = token_program,
         address = offer.ask_mint,
     )]
-    pub asked_mint: InterfaceAccount<'info, Mint>,
+    pub asked_mint: InterfaceAccount<'info, token_interface::Mint>,
 
-    pub token_program: Interface<'info, TokenInterface>,
+    pub token_program: Interface<'info, token_interface::TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
 }
 
@@ -157,12 +220,12 @@ pub struct Initialize<'info> {
     #[account(
         mint::token_program=token_program,
     )]
-    pub offer_mint: InterfaceAccount<'info, Mint>,
+    pub offer_mint: InterfaceAccount<'info, token_interface::Mint>,
 
     #[account(
         mint::token_program=token_program,
     )]
-    pub ask_mint: InterfaceAccount<'info, Mint>,
+    pub ask_mint: InterfaceAccount<'info, token_interface::Mint>,
 
     #[account(
         init_if_needed,
@@ -171,17 +234,17 @@ pub struct Initialize<'info> {
         associated_token::authority = offer,
         associated_token::token_program = token_program,
     )]
-    pub vault: InterfaceAccount<'info, TokenAccount>,
+    pub vault: InterfaceAccount<'info, token_interface::TokenAccount>,
 
     #[account(
         mut,
         associated_token::mint = offer_mint,
-        associated_token::authority = initializer.key(),
+        associated_token::authority = initializer,
         associated_token::token_program = token_program,
     )]
-    pub initializer_ata: InterfaceAccount<'info, TokenAccount>,
+    pub initializer_ata: InterfaceAccount<'info, token_interface::TokenAccount>,
 
-    pub token_program: Interface<'info, TokenInterface>,
+    pub token_program: Interface<'info, token_interface::TokenInterface>,
     pub system_program: Program<'info, System>,
     pub associated_token_program: Program<'info, AssociatedToken>,
 }
--- week2/escrow/tests/escrow.ts
+++ week2/escrow/tests/escrow.ts
@@ -1,9 +1,10 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Escrow } from "../target/types/escrow";
-import { Account, ASSOCIATED_TOKEN_PROGRAM_ID, createMint, getAccount, getAssociatedTokenAddress, getOrCreateAssociatedTokenAccount, Mint, mintTo, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import { Account, ASSOCIATED_TOKEN_PROGRAM_ID, createMint, getAccount, getAssociatedTokenAddress, getMint, getOrCreateAssociatedTokenAccount, Mint, mintTo, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
 import { assert } from "chai";
 
+const ASSOCIATED_TOKEN_2022_PROGRAM_ID = new anchor.web3.PublicKey("9W5GgxG42U9J5NdA4xj7Hb7sEvM2kQEP7RHNd5VFFPZN");
 
 describe("escrow", () => {
   // Configure the client to use the local cluster.
@@ -20,9 +21,13 @@ describe("escrow", () => {
   const askMintDecimals = 4;
 
   let vault: Account;
-  //let offer: anchor.web3.PublicKey;
+  let offer: anchor.web3.PublicKey;
+  let offerBump: number;
 
-  it("initialized escrow", async () => {
+  let offerAmnt: anchor.BN;
+  let askAmnt: anchor.BN;
+
+  it("initialized offer", async () => {
     offerMintAddr = await createMint(
       connection,
       wallet.payer,
@@ -33,7 +38,7 @@ describe("escrow", () => {
       { commitment: "confirmed" },
       TOKEN_2022_PROGRAM_ID
     );
-    console.log("offerMintAddr:", offerMintAddr);
+    //console.log("offerMintAddr:", offerMintAddr);
 
     askMintAddr = await createMint(
       connection,
@@ -45,10 +50,10 @@ describe("escrow", () => {
       { commitment: "confirmed" },
       TOKEN_2022_PROGRAM_ID
     );
-    console.log("askMintAddr:", askMintAddr);
+    //console.log("askMintAddr:", askMintAddr);
 
     const offerMint = await connection.getAccountInfo(offerMintAddr);
-    console.log("offerMint:", offerMint);
+    //console.log("offerMint:", offerMint);
 
     const initializerAta = await getOrCreateAssociatedTokenAccount(
       connection,
@@ -60,6 +65,18 @@ describe("escrow", () => {
       null,
       TOKEN_2022_PROGRAM_ID
     );
+
+    const initializerRecievingAta = await getOrCreateAssociatedTokenAccount(
+      connection,
+      wallet.payer,
+      askMintAddr,
+      wallet.publicKey,
+      null,
+      null,
+      null,
+      TOKEN_2022_PROGRAM_ID
+    );
+    //console.log("testing ata create or get");
 
     await mintTo(
       connection,
@@ -73,8 +90,8 @@ describe("escrow", () => {
       TOKEN_2022_PROGRAM_ID
     );
 
-    const offerAmnt = new anchor.BN(10);
-    const askAmnt = new anchor.BN(20);
+    offerAmnt = new anchor.BN(10);
+    askAmnt = new anchor.BN(20);
 
     const tx = await program.methods
       .initialize(new anchor.BN(offerId), offerAmnt, askAmnt)
@@ -86,15 +103,15 @@ describe("escrow", () => {
       })
       .rpc({ commitment: "confirmed" });
 
-    console.log("initiate tx successfull!", tx);
+    //console.log("initiate tx successfull!", tx);
 
     const latestBlockhash = await connection.getLatestBlockhash();
     await connection.confirmTransaction({ signature: tx, ...latestBlockhash }, "confirmed");
 
     const txDetails = await connection.getParsedTransaction(tx, "confirmed");
-    console.log(txDetails?.meta?.logMessages);
+    //console.log(txDetails?.meta?.logMessages);
 
-    const [offer, bump] = anchor.web3.PublicKey.findProgramAddressSync(
+    [offer, offerBump] = anchor.web3.PublicKey.findProgramAddressSync(
       [Buffer.from("offer"), new anchor.BN(offerId).toArrayLike(Buffer, 'le', 8)],
       program.programId
     );
@@ -108,9 +125,89 @@ describe("escrow", () => {
     );
     vault = await getAccount(connection, vaultAddr, undefined, TOKEN_2022_PROGRAM_ID);
 
-    console.log("vault account:", vault);
+    //console.log("vault account:", vault);
 
     assert.ok(vault.mint.equals(offerMintAddr), "Unexpected vault mint");
     assert.strictEqual(vault.amount.toString(), offerAmnt.toString(), "Unexpected vault amount");
   });
+
+  it("accept offer", async () => {
+    const offerAccnt = await program.account.offer.fetch(offer);
+
+    //console.log("offerAccount:", offerAccnt);
+
+    const keypair = anchor.web3.Keypair.generate();
+    const airdropTx = await connection.requestAirdrop(keypair.publicKey, 1 * Math.pow(10, 9));
+    const recentBlockhash = await connection.getLatestBlockhash();
+    await connection.confirmTransaction(
+      {
+        blockhash: recentBlockhash.blockhash,
+        lastValidBlockHeight: recentBlockhash.lastValidBlockHeight,
+        signature: airdropTx
+      },
+      "confirmed"
+    );
+
+    const acceptorsBal = await connection.getBalance(keypair.publicKey);
+    //console.log("acceptor balance", acceptorsBal);
+
+    const askMintAccntInfo = await getMint(connection, askMintAddr, "confirmed", TOKEN_2022_PROGRAM_ID);
+    //console.log("ask mint account info", askMintAccntInfo);
+
+    //console.log("wallet pubkey:", wallet.publicKey);
+
+    const initializerRecievingAta = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      askMintAddr,
+      keypair.publicKey,
+      null,
+      null,
+      null,
+      TOKEN_2022_PROGRAM_ID
+    );
+    //console.log("testing ata create or get");
+
+    const acceptorsSendingAta = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      askMintAddr,
+      keypair.publicKey,
+      null,
+      "confirmed",
+      null,
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    //console.log("acceptors's asked ata:", acceptorsSendingAta);
+
+    await mintTo(
+      connection,
+      wallet.payer,
+      askMintAddr,
+      acceptorsSendingAta.address,
+      wallet.payer,
+      100 * Math.pow(10, askMintAccntInfo.decimals),
+      undefined,
+      { commitment: "confirmed" },
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    //console.log("asked token minted to acceptors asked ata");
+
+    const tx = await program.methods.accept(new anchor.BN(offerId))
+      .accounts(
+        {
+          acceptor: keypair.publicKey,
+          askedMint: askMintAddr,
+          initializer: offerAccnt.user,
+          offeredMint: offerMintAddr,
+          tokenProgram: TOKEN_2022_PROGRAM_ID
+        }
+      )
+      .signers([keypair])
+      .rpc();
+
+    console.log(`tx success - ${tx}`);
+  })
 });
